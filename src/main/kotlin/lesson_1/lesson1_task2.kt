8984505c4c940aa297ee4a609233a6dfc38ea7bb
evgeniypetrov <ev.petrov@icloud.com>
2024-05-01 21:36:46 +0300

    KS-1-2 добавил вывод объявленных переменных, переменную для хранения количества работников и её вывод

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_1
+
+fun main() {
+    val numberOfOrders: Int = 75
+    val gratitudeText: String = "Благодарим за покупку!"
+
+    println("Количество заказов: $numberOfOrders")
+    println(gratitudeText)
+
+    var amountOfWorkers: Int = 2000
+//    println("Количество работников: $amountOfWorkers")
+
+    amountOfWorkers = 1999
+    println("Количество работников: $amountOfWorkers")
+}
