e2e1b7341f65868e919311bb9821b9c81a080287
evgeniypetrov <ev.petrov@icloud.com>
2024-05-01 21:51:40 +0300

    KS-1-3 добавил переменные для хранения данных о полете и их вывод

---
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,19 @@
+package org.example.lesson_1
+
+fun main() {
+    val year: Int = 1961
+    var hour: Int = 9
+    var minute: Int = 7
+
+    println("Год: $year")
+    println("Час: $hour")
+    println("Минута: $minute")
+
+    hour = 10
+    minute = 55
+
+    print("Время посадки: ")
+    print(hour)
+    print(':')
+    print(minute)
+}
