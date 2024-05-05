53ad958e09ad7d98382b28a12347beebcf708143
evgeniypetrov <ev.petrov@icloud.com>
2024-05-05 10:33:36 +0300

    KS-2-4 правки после ревью: добавил хранение бафа в процентах

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -3,7 +3,9 @@ package org.example.lesson_2
 fun main() {
     val amountOfCrystal: Byte = 7
     val amountOfIron: Byte = 11
-    val buffBonus = 0.2f
+    val buffPercent = 20f
+    val divider: Byte = 100
+    val buffBonus = buffPercent / divider
 
     val crystalBonus = amountOfCrystal * buffBonus
     val ironBonus = amountOfIron * buffBonus
