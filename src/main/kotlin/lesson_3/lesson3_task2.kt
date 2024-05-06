8834a963ba49a7dd613c3c9da8574a03237acd52
evgeniypetrov <ev.petrov@icloud.com>
2024-05-06 22:05:25 +0300

    KS-3-2 вывел ФИО и возраст

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_3
+
+fun main() {
+    var age: Byte = 20
+    val name = "Татьяна"
+    var surname = "Андреева"
+    val patronymic = "Сергеевна"
+
+    println("$surname $name $patronymic, $age")
+
+    age = 22
+    surname = "Сидорова"
+
+    println("$surname $name $patronymic, $age")
+}
