19bf627d5b322c26c6b983ebdc300cdf672d9366
evgeniypetrov <ev.petrov@icloud.com>
2024-05-11 21:44:13 +0300

    KS-4-5 правки после ревью: добавил ввод данных из консоли и вынес в константы Boolean переменные

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -3,16 +3,29 @@ package org.example.lesson_4
 const val MIN_CREW = 55
 const val RECOMMENDED_CREW = 70
 const val MIN_PROVISION = 50
+const val IS_CASE_DAMAGE = true
+const val IS_CASE_GOOD_WEATHER = true
 
 fun main() {
-    val isDamaged = true
-    val isGoodWeather = true
-    val crew = 70
-    val provisionAmount = 51
+    println(
+        """
+        Введите данные о корабле в следующем порядке:
+        1. Число экипажа
+        2. Количество провизии
+        3. Поврежден ли корабль
+        4. Благоприятная ли погода
+    """.trimIndent()
+    )
+
+    val crew = readln().toInt()
+    val provisionAmount = readln().toInt()
+    val isDamaged = readln().toBoolean()
+    val isGoodWeather = readln().toBoolean()
 
     val conditionsMet =
-        !isDamaged && (crew in MIN_CREW..RECOMMENDED_CREW) && (provisionAmount > MIN_PROVISION) ||
-                crew == RECOMMENDED_CREW && isGoodWeather && provisionAmount >= MIN_PROVISION
+        isDamaged != IS_CASE_DAMAGE && (crew in MIN_CREW..RECOMMENDED_CREW) && (provisionAmount > MIN_PROVISION) ||
+                isDamaged == IS_CASE_DAMAGE && crew == RECOMMENDED_CREW && isGoodWeather == IS_CASE_GOOD_WEATHER &&
+                provisionAmount >= MIN_PROVISION
 
     println("Корабль может выйти в плавание: $conditionsMet")
 }
