b10cfe2f3ed6128a5db8597f305fe73d4c8d7de7
evgeniypetrov <ev.petrov@icloud.com>
2024-05-16 20:19:03 +0300

    KS-7-2 написал генератор кода авторизации

---
+++ src/main/kotlin/lesson_7/lesson7_task2.kt
@@ -0,0 +1,21 @@
+package org.example.lesson_7
+
+const val CODE_FROM = 1000
+const val CODE_UNTIL = 9999
+
+fun main() {
+    var correctInput = false
+    val codeRange = CODE_FROM..CODE_UNTIL
+
+    while (!correctInput) {
+        val newCode = codeRange.random()
+        println("Ваш код авторизации: $newCode")
+
+        println("Введите код авторизации")
+        val input = readln().toInt()
+        if (input == newCode) {
+            correctInput = true
+            println("Добро пожаловать!")
+        }
+    }
+}
