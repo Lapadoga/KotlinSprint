73fa3a48baed3d736a583751c8856d8c92ba2947
evgeniypetrov <ev.petrov@icloud.com>
2024-05-20 12:07:58 +0300

    KS-8-5 правки после ревью: сразу инициализирую и объявляю массив

--- src/main/kotlin/lesson_8/lesson8_task5.kt
+++ src/main/kotlin/lesson_8/lesson8_task5.kt
@@ -1,15 +1,8 @@
 package org.example.lesson_8
 
 fun main() {
-    println("Введите количество ингредиентов")
-    val amountOfIngredients = readln().toInt()
-    if (amountOfIngredients == 0)
-        return
-    val ingredientsArray = Array(amountOfIngredients) { " " }
-
-    println("Введите ингредиенты")
-    for (i in ingredientsArray.indices)
-        ingredientsArray[i] = readln()
+    println("Введите количество ингредиентов, затем ингредиенты")
+    val ingredientsArray = Array(readln().toInt()) { readln() }
 
     println("Введенные ингредиенты: ${ingredientsArray.joinToString(", ")}")
 }
