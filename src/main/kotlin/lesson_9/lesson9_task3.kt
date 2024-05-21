637c6e020d1280f6a4cad6e147be2d5c37c04042
evgeniypetrov <ev.petrov@icloud.com>
2024-05-21 22:09:47 +0300

    KS-9-3 написал расчет количества ингредиентов

---
+++ src/main/kotlin/lesson_9/lesson9_task3.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_9
+
+fun main() {
+    val amountOfIngredientsList = mutableListOf(2, 50, 15)
+
+    println("Введите количество порций")
+    val amountOfServings = readln().toInt()
+    amountOfIngredientsList.replaceAll {
+        it * amountOfServings
+    }
+
+    val servingString = if (amountOfServings == 1) "порции" else "порций"
+    val ingredientsString = "Яиц - ${amountOfIngredientsList[0]}, молока - ${amountOfIngredientsList[1]}, " +
+            "сливочного масла - ${amountOfIngredientsList[2]}"
+    println("Для $amountOfServings $servingString вам понадобится: $ingredientsString")
+}
