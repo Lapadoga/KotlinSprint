48998b5cab0cb39e0483d60c3fcbe436860d860d
zhenya <ev.petrov@icloud.com>
2024-06-09 15:30:03 +0300

    KS-12-3 написал конструктор с конвертацией температуры

---
+++ src/main/kotlin/lesson_12/lesson12_task3.kt
@@ -0,0 +1,21 @@
+package org.example.lesson_12
+
+import kotlin.math.roundToInt
+
+const val KelvinCoefficient = 273.15
+
+fun main() {
+    val day = DayWeather2(275, 273, false)
+
+    day.print()
+}
+
+class DayWeather2(_dayTemperature: Int, _nightTemperature: Int, _wasPrecipitation: Boolean) {
+    var dayTemperature = (_dayTemperature - KelvinCoefficient).roundToInt()
+    var nightTemperature = (_nightTemperature - KelvinCoefficient).roundToInt()
+    var wasPrecipitation = _wasPrecipitation
+
+    fun print() {
+        println("Дневная температура: $dayTemperature, ночная температура: $nightTemperature, были осадки: $wasPrecipitation")
+    }
+}
