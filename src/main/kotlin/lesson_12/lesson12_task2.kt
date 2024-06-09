2a8b2b6fee0eff07de86bb45fbd9a59de7cf3d62
zhenya <ev.petrov@icloud.com>
2024-06-09 14:36:20 +0300

    KS-12-2 переименовал класс

--- src/main/kotlin/lesson_12/lesson12_task2.kt
+++ src/main/kotlin/lesson_12/lesson12_task2.kt
@@ -1,12 +1,12 @@
 package org.example.lesson_12
 
 fun main() {
-    val day = DayWeather(5, 2, true)
+    val day = DayWeather1(5, 2, true)
 
     day.print()
 }
 
-class DayWeather(_dayTemperature: Int, _nightTemperature: Int, _wasPrecipitation: Boolean) {
+class DayWeather1(_dayTemperature: Int, _nightTemperature: Int, _wasPrecipitation: Boolean) {
     var dayTemperature = _dayTemperature
     var nightTemperature = _nightTemperature
     var wasPrecipitation = _wasPrecipitation
