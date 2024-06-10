cccc2ec482ac76f391960c88f89b349be0351b05
zhenya <ev.petrov@icloud.com>
2024-06-10 21:40:19 +0300

    KS-12-1 правка после ревью: дописал задачу в соответствии с ТЗ и поправил вывод пользователю

--- src/main/kotlin/lesson_12/lesson12_task1.kt
+++ src/main/kotlin/lesson_12/lesson12_task1.kt
@@ -4,9 +4,13 @@ fun main() {
     val day1 = DayWeather()
     val day2 = DayWeather()
 
+    day1.dayTemperature = 4
+    day1.nightTemperature = -1
     day1.wasPrecipitation = false
 
     day2.dayTemperature = 5
+    day2.nightTemperature = -4
+    day2.wasPrecipitation = true
 
     day1.print()
     day2.print()
@@ -18,6 +22,7 @@ class DayWeather {
     var wasPrecipitation = true
 
     fun print() {
-        println("Дневная температура: $dayTemperature, ночная температура: $nightTemperature, были осадки: $wasPrecipitation")
+        val rainyLiteral = if (wasPrecipitation) "Да" else "Нет"
+        println("Дневная температура: $dayTemperature, ночная температура: $nightTemperature, были осадки: $rainyLiteral")
     }
 }
