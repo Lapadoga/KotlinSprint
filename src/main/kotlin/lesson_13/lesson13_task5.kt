9b992b9f31707f516025357027a9e270a743bd93
zhenya <ev.petrov@icloud.com>
2024-06-12 18:34:15 +0300

    KS-13-5 добавил обработку ошибки при преобразовании ввода

---
+++ src/main/kotlin/lesson_13/lesson13_task5.kt
@@ -0,0 +1,31 @@
+package org.example.lesson_13
+
+fun main() {
+    println("Введите имя контакта")
+    val name = readln()
+
+    println("Введите компанию контакта")
+    var company: String? = readln()
+    if (company == "")
+        company = null
+
+    println("Введите номер контакта")
+    try {
+        val number = readln().toLong()
+
+        val user = UserCredits4(name, number, company)
+        user.print()
+    } catch (e: Exception) {
+        println(e)
+    }
+}
+
+class UserCredits4(
+    val name: String,
+    val phoneNumber: Long,
+    val company: String? = null,
+) {
+    fun print() {
+        println("Имя: $name, номер: $phoneNumber, компания: ${company ?: "<не указано>"}")
+    }
+}
