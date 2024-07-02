8f63ab3f72dd1bb3ce0b4f23bb7b7550325a88c5
zhenya <ev.petrov@icloud.com>
2024-07-02 20:29:45 +0300

    KS-17-3 написал класс папки и переопределил геттеры

---
+++ src/main/kotlin/lesson_17/lesson17_task3.kt
@@ -0,0 +1,19 @@
+package org.example.lesson_17
+
+fun main() {
+    val folder = Folder("folder1", 10, true)
+    println(folder.name)
+    println(folder.numberOfFiles)
+
+    folder.isSecret = false
+    println(folder.name)
+    println(folder.numberOfFiles)
+}
+
+class Folder(folderName: String, numberOfFiles_: Int, var isSecret: Boolean) {
+    val name = folderName
+        get() = if (isSecret) "Скрытая папка" else field
+
+    val numberOfFiles = numberOfFiles_
+        get() = if (isSecret) 0 else field
+}
