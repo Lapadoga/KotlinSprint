d038c7666c72d23ab9e18ca5f2726eea6870b166
zhenya <ev.petrov@icloud.com>
2024-07-03 12:56:31 +0300

    KS-17-3 правка после ревью: изменил модификатор доступа к свойству

--- src/main/kotlin/lesson_17/lesson17_task3.kt
+++ src/main/kotlin/lesson_17/lesson17_task3.kt
@@ -4,13 +4,9 @@ fun main() {
     val folder = Folder("folder1", 10, true)
     println(folder.name)
     println(folder.numberOfFiles)
-
-    folder.isSecret = false
-    println(folder.name)
-    println(folder.numberOfFiles)
 }
 
-class Folder(folderName: String, numberOfFiles_: Int, var isSecret: Boolean) {
+class Folder(folderName: String, numberOfFiles_: Int, private var isSecret: Boolean) {
     val name = folderName
         get() = if (isSecret) "Скрытая папка" else field
 
