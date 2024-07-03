727ebea312229c17a9b0f6d19bb032dda203e56c
zhenya <ev.petrov@icloud.com>
2024-07-03 12:54:22 +0300

    KS-17-1 правка после ревью: убрал значение по умолчанию у свойства

--- src/main/kotlin/lesson_17/lesson17_task1.kt
+++ src/main/kotlin/lesson_17/lesson17_task1.kt
@@ -1,10 +1,10 @@
 package org.example.lesson_17
 
-class QuizElement(questionText: String) {
+class QuizElement(questionText: String, answerText: String) {
     val question = questionText
         get() = field
 
-    var answer = ""
+    var answer: String = answerText
         get() = field
         set(value) {
             field = value
