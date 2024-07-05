1b4a3e4c57b4325efd015b5c781067256baec2eb
zhenya <ev.petrov@icloud.com>
2024-07-05 12:29:27 +0300

    KS-19-3 описал класс космического корабля без реализации его методов

---
+++ src/main/kotlin/lesson_19/lesson19_task3.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_19
+
+class Spaceship {
+    fun land() {
+        TODO()
+    }
+
+    fun launch() {
+        TODO()
+    }
+
+    fun shootAsteroids() {
+        TODO("нужна информация о дополнительной логике")
+        // TODO нужна информация о дополнительной логике
+    }
+}
