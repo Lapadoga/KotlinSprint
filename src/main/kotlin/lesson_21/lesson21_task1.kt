5f99f435fa481e8b9089b76475913461a904d99f
zhenya <ev.petrov@icloud.com>
2024-07-07 11:34:12 +0300

    KS-21-1 написал расширение для подсчета количества гласных букв в строке

---
+++ src/main/kotlin/lesson_21/lesson21_task1.kt
@@ -0,0 +1,19 @@
+package org.example.lesson_21
+
+fun main() {
+    println("Введите проверямемую строку")
+    val string = readln()
+    println(string.vowelCount())
+}
+
+fun String.vowelCount(): Int {
+    val englishVowels = listOf('a', 'e', 'i', 'o', 'u', 'y')
+    val russianVowels = listOf('а', 'у', 'о', 'и', 'э', 'ы', 'я', 'ю', 'е', 'ё')
+
+    var counter = 0
+    this.forEachIndexed { _, c ->
+        if (englishVowels.contains(c.lowercaseChar()) || russianVowels.contains(c.lowercaseChar()))
+            counter++
+    }
+    return counter
+}
