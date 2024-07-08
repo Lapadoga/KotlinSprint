507f13434ef925513003d7384ecb55b69697ffa8
zhenya <ev.petrov@icloud.com>
2024-07-08 17:05:51 +0300

    KS-22-3 правка после ревью: использую деструктуризацию

--- src/main/kotlin/lesson_22/lesson22_task3.kt
+++ src/main/kotlin/lesson_22/lesson22_task3.kt
@@ -3,9 +3,7 @@ package org.example.lesson_22
 fun main() {
     val house = House(30, 40, 50)
 
-    val length = house.component1()
-    val width = house.component2()
-    val height = house.component3()
+    val (length, width, height) = house
 
     println(length)
     println(width)
